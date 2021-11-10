37e93d0c3e06bf09defb049e4c6a1b9541ff165b
Rovijun <rovijun@gmail.com>
2021-11-10 14:06:58 +0100

    Repositories insertions

---
+++ src/main/java/com/jeux/repositories/AvisRepository.java
@@ -0,0 +1,15 @@
+package com.jeux.repositories;
+
+import com.jeux.models.Avis;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AvisRepository extends CrudRepository<Avis, Long> {
+    @Override
+    List<Avis> findAll();
+
+    List<Avis> findByJeuId(Long jeuId);
+}
